27347957ae5b2335fa305c4da2219b23107dca3e
Chris Masawi <masawichriskev@gmail.com>
2023-07-18 16:06:31 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the file name it was compiled from, & append '\0'
+ *
+ * Return: exit_success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
