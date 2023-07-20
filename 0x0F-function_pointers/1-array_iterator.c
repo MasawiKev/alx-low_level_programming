ea251d9dbcfe4120f59d0a87567f71328483dffd
Chris Masawi <masawichriskev@gmail.com>
2023-07-20 10:40:53 -0700

    learning function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+  * array_iterator - This prints each array element on a new line
+  * @array: the array
+  * @size: the size of array
+  * @action: a pointer to print in regular or hex
+  * Return: void
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
+
