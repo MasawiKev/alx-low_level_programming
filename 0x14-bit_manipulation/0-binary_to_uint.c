0e9a4177deebed06ed6c9b53d4c9a61d42aa30af
Masawi <Masawichriskev@gmail.com>
2023-08-31 18:38:46 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int deci_val = 0;
+	unsigned int uni;
+	int len, btwo;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	uni = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, btwo = 1; len >= 0; len--, btwo *= 2)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		deci_val = 2 * deci_val + (b[i] - '0');
+		}
+
+		if (b[len] & 1)
+		{
+			uni += btwo;
+		}
 	}
 
-	return (dec_val);
+	return (uni);
 }
