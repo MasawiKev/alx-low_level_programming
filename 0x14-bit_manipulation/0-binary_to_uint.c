77dffdb016659a9cf797bf638820d1ef7b92acd6
Masawi <Masawichriskev@gmail.com>
2023-08-31 18:31:28 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,25 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * _strlen - finds the length of a string
- * @s: pointer to the string
- * Return: void
-*/
-
-
-int _strlen(const char *s)
-{
-	int i = 0;
-	while (s[i])
-		i++;
-
-	return (i);
-}
-
-/**
-* binary_to_unit - converts a binary number to an unsigned int.
-* @b: string containing the binary number
-*
-* Return: the converted number
-*/
-
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	int i, len;
+	int i;
+	unsigned int deci_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	len = _strlen(b);
-
-	for (i = 0; i != len; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[len - i - 1] == '1')
-			n += 1 << i;
-		else if (b[len - i - 1] != '0')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		deci_val = 2 * deci_val + (b[i] - '0');
 	}
 
-	return (n);
+	return (dec_val);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * clear_bit - sets the value of a bit to 0 at a given index.
- * @n: Pointer to the number
+ * clear_bit - sets the value of a given bit to 0
+ * @n: pointer to the number to change
  * @index: index of the bit to clear
  *
- * Return: 1 for success and -1 for fail.
+ * Return: 1 for success and -1 for failure
  */
-
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (indec > 63)
+	if (index > 63)
 		return (-1);
 
 	*n = (~(1UL << index) & *n);
