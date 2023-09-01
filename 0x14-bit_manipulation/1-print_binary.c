94475c367de7e07e4a712a891765e26c0f8be7cc
Masawi <Masawichriskev@gmail.com>
2023-08-31 18:15:02 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,21 +6,21 @@
  */
 void print_binary(unsigned long int n)
 {
-        int i, count = 0;
-        unsigned long int curr;
+	int i, count = 0;
+	unsigned long int curr;
 
-        for (i = 63; i >= 0; i--)
-        {
-                curr = n >> i;
+	for (i = 63; i >= 0; i--)
+	{
+		curr = n >> i;
 
-                if (curr & 1)
-                {
-                        _putchar('1');
-                        count++;
-                }
-                else if (count)
-                        _putchar('0');
-        }
-        if (!count)
-                _putchar('0');
+ 		if (curr & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,16 +9,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-        int i, count = 0;
-        unsigned long int curr;
-        unsigned long int exc = n ^ m;
+	int i, count = 0;
+	unsigned long int curr;
+	unsigned long int exc = n ^ m;
 
-        for (i = 63; i >= 0; i--)
-        {
-                curr = exc >> i;
-                if (curr & 1)
-                        count++;
-        }
+	for (i = 63; i >= 0; i--)
+	{
+		curr = exc >> i;
+		if (curr & 1)
+			count++;
+	}
 
-        return (count);
+	return (count);
 }
